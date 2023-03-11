f331a3b48016f852c773053150b20e9133d21f95
kejsik <90483151+kejsik@users.noreply.github.com>
2023-03-11 02:13:56 -0800

    Create sigs.cpp

---
+++ sigs.cpp
@@ -0,0 +1,3 @@
+ProjectWorldToScreen: 40 53 55 56 57 41 56 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 33 DB 45 8A F1 49 8B F8 48 8B EA 48 8B F1 48 85 C9 0F 84
+
+That's the only thing that have changed in new update for internal's.

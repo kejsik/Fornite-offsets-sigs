c2f78ae8058b744abf3cf0bcb35b2b86b17dc40f
kejsik <90483151+kejsik@users.noreply.github.com>
2023-03-20 20:50:36 +0000

    Update sigs.cpp

--- sigs.cpp
+++ sigs.cpp
@@ -1,5 +1,5 @@
 ProjectWorldToScreen: 40 53 55 56 57 41 56 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 33 DB 45 8A F1 49 8B F8 48 8B EA 48 8B F1 48 85 C9 0F 84
-UWorld : 48 89 05 ? ? ? ? 48 8B 4B 78
+UWorld : 48 89 05 ?? ?? ?? ?? 0F 28 D7
 
 GObject : 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1
 
@@ -9,4 +9,4 @@ GetNameByIndex : 40 53 48 83 EC 20 33 C0 48 8B DA 48 89 02 48 89 42 08 E8 ? ? ? 
 
 BoneMatrix : E8 ? ? ? ? 0F 10 40 68
 
-Only GetNameByIndex changed.
+Only Uworld and GetNameByIndex changed.

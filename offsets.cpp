e0ec3477737a8b570fe3b8475fe08db21bdeb11b
kejsik <90483151+kejsik@users.noreply.github.com>
2023-03-25 03:53:46 +0000

    Update offsets.cpp

--- offsets.cpp
+++ offsets.cpp
@@ -1,60 +1,65 @@
-#define GWORLD = 0xEEDBD10
-#define FNAME = 0xEEFE2C0
-#define RESULT_DA = 0xEE02330
+ 
+define GWORLD = 0xEEDAB90
+#define FNAME = 0xEEFD140
+#define RESULT_DA = 0xEE011E0
+
 
 -DECRYPT ANSI-
-NOT TESTED
-uintptr_t FNameOffset = 0xEEFE2C0;
-uintptr_t value = 0xEE02330;
+uintptr_t FNameOffset = 0xEEFD140;
+uintptr_t value = 0xEE011E0;
 
-         char* v2; // rdi
-        unsigned int v4; // ebx
-        __int64 result; // rax
-        unsigned int v6; // edx
-        unsigned int v7; // er8
+ char *v2; // rdi
+  int v4; // ebx
+  __int16 result; // ax
+  int v6; // edx
+  int v7; // ecx
+  int v8; // eax
+  unsigned int v9; // ecx
 
-char* v2; // rbx
-        int v4; // edi
-        unsigned int v5; // eax
-        __int64 result; // rax
-        int v7; // er8
-        unsigned int v8; // edx
+  v2 = buffer;
+  v4 = Lenght;
+  result = read<unsigned int>(module + value);
+  v6 = 0;
+  v7 = 28;
+  if ( v4 )
+  {
+    do
+    {
+      v8 = v6++;
+      v9 = (v8 | 0xB000) + v7;
+      result = v9 ^ ~*v2;
+      v7 = v9 >> 2;
+      *v2++ = result;
+    }
+    while ( v6 < v4 );
+  }
 
-        v2 = buffer;
 
-        v4 = Lenght;
-
-        result = read<unsigned int>(module + value) >> 5;
-        v7 = 0;
-        if (v4)
-        {
-            do
-            {
-                v8 = *v2++;
-                result = (unsigned int)(v7 + result + 96);
-                ++v7;
-                *(v2 - 1) = (16 * v8) | (result ^ (v8 >> 4)) & 0xF;
-            } while (v7 < v4);
-        } } while (result);
-        }
-
-Gworld : 0xEEDBD10
-FName : 0xwaitIDA
-GameInstance : 0x1B8
+Gworld : 0xEEDAB90
+FName : 0xEEFD140
 LocalPlayerArray : 0x38
 PlayerController : 0x30
+GameInstance : 0x1B8
 AcknowledgedPawn : 0x330
 RootComponent : 0x190
+
 RelativeLocation : 0x128
 TeamIndex : 0x10A0
 CurrentWeapon : 0x8F8
 PlayerState : 0x2A8
 Mesh : 0x310
-ItemDefinition : 0x18
-Tier : 0x73
+WeaponData : 0x3F8
+AmmoCount : 0xB8C
 DisplayName : 0x90
+bADSWhileNotOnGround : 0x4DF6
+Levels : 0x170
+PlayerCameraManager : 0x340
+LastFireTime : 0xAD8
+LastFireTimeVerified : 0xADC
+CurrentVehicle : 0x2428
+ReviveFromDBNOTime : 0x44A0
+bIsDBNO : 0x832
 Bones Array : 0x5F8
 Bones cache : 0x608
 
-
-credits to efk
+Credit to efk

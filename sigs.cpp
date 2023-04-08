48d18b827ccd91c375e4e3ca1fc5478d903c8e3e
kejsik <90483151+kejsik@users.noreply.github.com>
2023-04-08 22:08:01 +0000

    Update sigs.cpp

--- sigs.cpp
+++ sigs.cpp
@@ -1,13 +1,19 @@
 ProjectWorldToScreen: 40 53 55 56 57 41 56 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 33 DB 45 8A F1 49 8B F8 48 8B EA 48 8B F1 48 85 C9 0F 84
-UWorld : 48 89 05 ?? ?? ?? ?? 0F 28 D7
 
 GObject : 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1
 
 FreeFN : 48 89 D1 48 FF 25 ? ? ? ? C3
 
-GetNameByIndex 40 53 48 83 EC 20 33 C0 48 8B DA 48 89 02 48 89 42 08 E8 ? ? ? ? 48 8B C3 48 83 C4 20 5B C3
+Gworld : 48 89 05 ? ? ? ? 0F 28 D7
 
 BoneMatrix : E8 ? ? ? ? 0F 10 40 68
 
+Getnamebyindex : 40 53 48 83 EC 20 33 C0 48 8B DA 48 89 02 48 89 42 08 E8 ? ? ? ? 48 8B C3 48 83 C4 20 5B C3
+
 Decrypt ANSI : 48 89 5C 24 ? 57 48 83 EC 20 44 0F B7 01 48 8B FA 49 C1 E8 06 48 8D 51 04 48 8B D9 4D 03 C0 48 8B CF E8
 
+LineSightOfTo : 48 8B C4 48 89 58 20 55 56 57 41 54 41 56 48 8D 68 B8 48 81 EC ?? ?? ?? ??
+
+FNAME : 48 83 EC 28 80 3D ? ? ? ? ? 74 0C 48 8D 05 ? ? ? ? 48 83 C4 28 C3
+
+if FNAME = null - try with : 48 8D 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? ? 48 83 C4 28 C3
